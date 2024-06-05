57b37c7a6afb20a6e279b6dbd6a5d65bcef6490e
shrinidhi.upadhyaya <shrinidhi.upadhyaya@stud.uni-bamberg.de>
2024-06-05 23:28:09 +0200

    feat: kubernetes service apply delete scripts

---
+++ _kubernetes_setup/apply-services.sh
@@ -0,0 +1,10 @@
+@echo off
+echo Applying Kubernetes resources...
+
+kubectl apply -f booking-service-deployment.yml
+kubectl apply -f payment-service-deployment.yml
+kubectl apply -f notification-service-deployment.yml
+kubectl apply -f nginx-configmap-deployment.yml
+kubectl apply -f nginx-service-deployment.yml
+
+echo Kubernetes resources applied.
---
+++ _kubernetes_setup/delete-services.sh
@@ -0,0 +1,10 @@
+@echo off
+echo Deleting Kubernetes resources...
+
+kubectl delete -f nginx-service-deployment.yml
+kubectl delete -f nginx-configmap-deployment.yml
+kubectl delete -f booking-service-deployment.yml
+kubectl delete -f payment-service-deployment.yml
+kubectl delete -f notification-service-deployment.yml
+
+echo Kubernetes resources deleted.
