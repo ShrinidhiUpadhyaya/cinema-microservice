57710decf2e8cd17a042af38725fe711ca072d74
shrinidhi.upadhyaya <shrinidhi.upadhyaya@stud.uni-bamberg.de>
2024-06-05 22:58:25 +0200

    feat: nginx gateway kubernetes implementation

--- booking-service/src/services/payment.service.js
+++ booking-service/src/services/payment.service.js
@@ -4,7 +4,7 @@ process.env.NODE_TLS_ACCEPT_UNTRUSTED_CERTIFICATES_THIS_IS_INSECURE = "1";
 
 module.exports = (paymentOrder) => {
   return new Promise((resolve, reject) => {
-    supertest(process.env.PAYMENT_SERVICE)
+    supertest(process.env.NGINX_SERVICE)
       .post("/payment/makePurchase")
       .send({ paymentOrder })
       .end((err, res) => {
