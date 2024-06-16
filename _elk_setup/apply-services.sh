921171a2fdccd2d5bed29dc2f0a5db5459592be2
shrinidhi.upadhyaya <shrinidhi.upadhyaya@stud.uni-bamberg.de>
2024-06-16 10:55:29 +0200

    feat(booking-service): elk integration

---
+++ _elk_setup/apply-services.sh
@@ -0,0 +1,9 @@
+@echo off
+echo Applying Kubernetes resources...
+
+kubectl apply -f kibana-config.yml
+kubectl apply -f kibana-deployment.yml
+kubectl apply -f logstash-config.yml
+kubectl apply -f logstash-deployment.yml
+
+echo Kubernetes resources applied.
---
+++ _elk_setup/delete-services.sh
@@ -0,0 +1,9 @@
+@echo off
+echo Deleting Kubernetes resources...
+
+kubectl delete -f kibana-config.yml
+kubectl delete -f kibana-deployment.yml
+kubectl delete -f logstash-config.yml
+kubectl delete -f logstash-deployment.yml
+
+echo Kubernetes resources deleted.
