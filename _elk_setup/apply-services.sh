44548d2fcc939e31d4ea24cf57a1ce4508ed2520
shrinidhi.upadhyaya <shrinidhi.upadhyaya@stud.uni-bamberg.de>
2024-06-26 10:34:51 +0200

    feat: apm init

--- _elk_setup/apply-services.sh
+++ _elk_setup/apply-services.sh
@@ -5,5 +5,6 @@ kubectl apply -f kibana-config.yml
 kubectl apply -f kibana-deployment.yml
 kubectl apply -f logstash-config.yml
 kubectl apply -f logstash-deployment.yml
+kubectl apply -f apm-deployment.yml
 
 echo Kubernetes resources applied.
--- _elk_setup/delete-services.sh
+++ _elk_setup/delete-services.sh
@@ -5,5 +5,6 @@ kubectl delete -f kibana-config.yml
 kubectl delete -f kibana-deployment.yml
 kubectl delete -f logstash-config.yml
 kubectl delete -f logstash-deployment.yml
+kubectl delete -f apm-deployment.yml
 
 echo Kubernetes resources deleted.
