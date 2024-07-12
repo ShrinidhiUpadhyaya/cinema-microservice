8a67e10ad907e60b30de1a158c5984f18f213a02
shrinidhi.upadhyaya <shrinidhi.upadhyaya@stud.uni-bamberg.de>
2024-07-12 20:24:30 +0200

    feat: pino-socket logstash implementation

--- booking-service/src/config/logger.js
+++ booking-service/src/config/logger.js
@@ -1,13 +1,35 @@
 const os = require("os");
-const logger = require("pino")({
-  level: process.env.LOG_LEVEL || "info",
-  base: {
-    application: process.env.LOG_APPLICATION || null,
-    version: process.env.LOG_VERSION || null,
-    environment: process.env.LOG_ENVIRONMENT || null,
-    pid: process.pid,
-    hostname: os.hostname(),
+const pino = require("pino");
+
+const transport = pino.transport({
+  target: "pino-socket",
+  options: {
+    address: "logstash",
+    port: 5044,
+    mode: "tcp",
   },
 });
+
+transport.on("socketError", (err) => {
+  console.log("Socket error", err);
+});
+
+transport.on("open", () => {
+  console.log("Connection established");
+});
+
+const logger = pino(
+  {
+    level: "trace",
+    base: {
+      application: process.env.LOG_APPLICATION || null,
+      version: process.env.LOG_VERSION || null,
+      environment: process.env.LOG_ENVIRONMENT || null,
+      pid: process.pid,
+      hostname: os.hostname(),
+    },
+  },
+  transport
+);
 
 module.exports = logger;
