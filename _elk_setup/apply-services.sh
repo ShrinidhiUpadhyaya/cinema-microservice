826d9c85d11411a83b365879b7f9da5b382dd1c7
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2024-09-07 23:55:51 +0200

    fix: elk deployment optimization

--- _elk_setup/apply-services.sh
+++ _elk_setup/apply-services.sh
@@ -1,9 +1,7 @@
 @echo off
 echo Applying Kubernetes resources...
 
-kubectl apply -f kibana-config.yml
 kubectl apply -f kibana-deployment.yml
-kubectl apply -f logstash-config.yml
 kubectl apply -f logstash-deployment.yml
 
 echo Kubernetes resources applied.
