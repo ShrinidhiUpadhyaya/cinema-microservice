d4454bc6e46f8b175ed88c22586388631f8f34fb
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2024-09-07 23:36:48 +0200

    feat: kafka implementation

--- _elk_setup/apply-services.sh
+++ _elk_setup/apply-services.sh
@@ -1,9 +1,11 @@
 @echo off
 echo Applying Kubernetes resources...
 
-kubectl apply -f kibana-config.yml
 kubectl apply -f kibana-deployment.yml
-kubectl apply -f logstash-config.yml
 kubectl apply -f logstash-deployment.yml
+
+#-------------------------------- Enable For Kafka --------------------------------
+#kubectl apply -f kafka-deployment.yml
+
 
 echo Kubernetes resources applied.
--- _elk_setup/delete-services.sh
+++ _elk_setup/delete-services.sh
@@ -1,9 +1,9 @@
 @echo off
 echo Deleting Kubernetes resources...
 
-kubectl delete -f kibana-config.yml
 kubectl delete -f kibana-deployment.yml
-kubectl delete -f logstash-config.yml
 kubectl delete -f logstash-deployment.yml
+#-------------------------------- Enable For Kafka --------------------------------
+#kubectl delete -f kafka-deployment.yml
 
 echo Kubernetes resources deleted.
