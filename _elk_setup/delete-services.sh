16fb088c7e29c9ac3e43b1d66db1975059d9c157
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2024-09-08 09:49:25 +0200

    fix: elk deployment optimization

--- _elk_setup/delete-services.sh
+++ _elk_setup/delete-services.sh
@@ -5,9 +5,4 @@ kubectl delete -f kibana-deployment.yml
 kubectl delete -f logstash-deployment.yml
 kubectl delete -f apm-deployment.yml
 
-kubectl delete -f apm-config.yml
-kubectl delete -f logstash-config.yml
-kubectl delete -f kibana-config.yml
-
-
 echo Kubernetes resources deleted.
