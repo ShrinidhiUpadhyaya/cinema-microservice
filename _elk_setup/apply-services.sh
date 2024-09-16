65317cd343fe89e18521deeaa20180056249f125
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2024-09-16 14:23:13 +0200

    refactor: elk deployment

--- _elk_setup/apply-services.sh
+++ _elk_setup/apply-services.sh
@@ -1,10 +1,6 @@
 @echo off
 echo Applying Kubernetes resources...
 
-kubectl apply -f kibana-config.yml
-kubectl apply -f apm-config.yml
-kubectl apply -f logstash-config.yml
-
 kubectl apply -f kibana-deployment.yml
 kubectl apply -f logstash-deployment.yml
 kubectl apply -f apm-deployment.yml
